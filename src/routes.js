099bf156dca31b55e4a4158c00d2b7df30c663bd
Jakab Edina <jakab_edina@yahoo.com>
2019-05-27 12:38:50 +0200

    loading special info on home page works

--- src/routes.js
+++ src/routes.js
@@ -12,8 +12,8 @@ routes.post('/register', userController.registerUser);
 routes.post('/login', userController.loginUser);
 
 //passing in the middleware
-routes.get('/special', passport.authenticate('jwt',{session:false}),(req, res) => {
-    return res.json({ msg:'Hej ${req.user.email}!'}); 
-})
+routes.get('/special', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.json({ msg: `Hey ${req.user.email}! I open at the close.` });
+});
 
 module.exports = routes;
