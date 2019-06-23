83b86159c947b3315140b9209b4a3f21518905fe
Jakab Edina <jakab_edina@yahoo.com>
2019-06-23 10:38:02 +0200

    exercises and progress works

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,64 +1,129 @@
-var User = require('../models/user');
-var jwt = require('jsonwebtoken');
-var config = require('../config/config');
- 
+var User = require("../models/user");
+var jwt = require("jsonwebtoken");
+var config = require("../config/config");
+
 function createToken(user) {
-    return jwt.sign({ id: user.id, email: user.email }, config.jwtSecret, {
-        expiresIn: 200 // 86400 expires in 24 hours
-      });
+  return jwt.sign({ id: user.id, email: user.email }, config.jwtSecret, {
+    expiresIn: 200 // 86400 expires in 24 hours
+  });
 }
- 
+
 exports.registerUser = (req, res) => {
-    if (!req.body.email || !req.body.password) {
-        return res.status(400).json({ 'msg': 'You need to send email and password' });
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ msg: "You need to send email and password" });
+  }
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return res.status(400).json({ msg: err });
     }
- 
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (err) {
-            return res.status(400).json({ 'msg': err });
-        }
- 
-        if (user) {
-            return res.status(400).json({ 'msg': 'The user already exists' });
-        }
- 
-        let newUser = User(req.body);
-        newUser.save((err, user) => {
-            if (err) {
-                return res.status(400).json({ 'msg': err });
-            }
-            return res.status(201).json(user);
-        });
+
+    if (user) {
+      return res.status(400).json({ msg: "The user already exists" });
+    }
+
+    let newUser = User(req.body);
+    newUser.save((err, user) => {
+      if (err) {
+        return res.status(400).json({ msg: err });
+      }
+      return res.status(201).json(user);
     });
+  });
 };
- 
+
 exports.loginUser = (req, res) => {
-    if (!req.body.email || !req.body.password) {
-        return res.status(400).send({ 'msg': 'You need to send email and password' });
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ msg: "You need to send email and password" });
+  }
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return res.status(400).send({ msg: err });
     }
- 
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (err) {
-            return res.status(400).send({ 'msg': err });
-        }
- 
-        if (!user) {
-            return res.status(400).json({ 'msg': 'The user does not exist' });
-        }
- 
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (isMatch && !err) {
-                return res.status(200).json({
-                    token: createToken(user)
-                });
-            } else {
-                return res.status(400).json({ msg: 'The email and password don\'t match.' });
-            }
+
+    if (!user) {
+      return res.status(400).json({ msg: "The user does not exist" });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (isMatch && !err) {
+        return res.status(200).json({
+          token: createToken(user)
         });
+      } else {
+        return res
+          .status(400)
+          .json({ msg: "The email and password don't match." });
+      }
     });
+  });
+};
+exports.addCompletedExercise = (req, res) => {
+  if (!req.body.exerciseId || !req.body.date || !req.body.email) {
+    return res.status(400).send({
+      msg:
+        "Invalid JSON Body - expected {exerciseId: string, date: Date, email: string, liked?: boolean}."
+    });
+  }
+
+  User.findOneAndUpdate(
+    { email: req.body.email },
+    {
+      $push: {
+        completed: {
+          exerciseId: req.body.exerciseId,
+          date: req.body.date,
+          name: req.body.name,
+          liked: req.body.liked
+        }
+      }
+    },
+    { upsert: true, strict: false, new:true },
+    (err, doc, success) => {
+      if (err) {
+        return res.status(400).send({ msg: err });
+      } else {
+        return res.status(200).json({
+          updatedUser: doc,
+          res: success
+        });
+      }
+    }
+  );
 };
 
-exports.userInfo =  (req, res) => {
-    return res.json({ msg: `Name ${req.user.email}` });
-};
+exports.addDizzyLog = (req, res) => {
+  if (!req.body.level || !req.body.date || !req.body.email) {
+    return res.status(400).send({
+      msg:
+        "Invalid JSON Body - expected {exerciseId: string, date: Date, email: string, liked?: boolean}."
+    });
+  }
 
+  User.findOneAndUpdate(
+    { email: req.body.email },
+    {
+      $push: {
+        dizzyLog: {
+          level: req.body.level,
+          date: req.body.date,
+        }
+      }
+    },
+    { upsert: true, strict: false, new:true },
+    (err, doc, success) => {
+      if (err) {
+        return res.status(400).send({ msg: err });
+      } else {
+        return res.status(200).json({
+          updatedUser: doc,
+          res: success
+        });
+      }
+    }
+  );
+};
+exports.userInfo = (req, res) => {
+  return res.json({ msg: `Name ${req.user.email}` });
+};
--- src/routes.js
+++ src/routes.js
@@ -1,78 +1,98 @@
-var express = require('express');
+var express = require("express");
 //express is for easily getting an instance of the router in all files and apply the routes
 var routes = express.Router();
-var userController = require('./controller/user-controller');
-var passport = require('passport');
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+var userController = require("./controller/user-controller");
+var passport = require("passport");
+var mongo = require("mongodb");
+var MongoClient = require("mongodb").MongoClient;
+var assert = require("assert");
 
-var url = 'mongodb+srv://edinaj:edinajpass@physapp-ejf1b.mongodb.net/ionic-jwt';
+var url = "mongodb+srv://edinaj:edinajpass@physapp-ejf1b.mongodb.net/ionic-jwt";
 
-routes.get('/', (req, res) => {
-    return res.send('Hello, this is the API!');
-})
-
-routes.get('/', function(req, res, next){
-    res.render('index');
+routes.get("/", (req, res) => {
+  return res.send("Hello, this is the API!");
 });
 
+routes.get("/", function(req, res, next) {
+  res.render("index");
+});
 
-routes.post('/register', userController.registerUser);
-routes.post('/login', userController.loginUser);
-
+routes.post("/register", userController.registerUser);
+routes.post("/login", userController.loginUser);
+routes.post("/add-completed-exercise", userController.addCompletedExercise);
+routes.post("/add-dizzy-log", userController.addDizzyLog);
 //passing in the middleware
-routes.get('/special', passport.authenticate('jwt', { session: false }), (req, res) => {
+routes.get(
+  "/special",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     return res.json({ msg: `Hey ${req.user.email}! I open at the close.` });
-});
+  }
+);
 
-routes.get('/get-users', function(req, res, next){
+routes.get("/get-users", function(req, res, next) {
+  var resultArray = [];
+
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db
+      .db("ionic-jwt")
+      .collection("users")
+      .find();
+    cursor.forEach(
+      function(doc, err) {
+        assert.equal(null, err);
+        resultArray.push(doc);
+      },
+      function() {
+        db.close();
+        res.json(resultArray);
+      }
+    );
+  });
+});
+test = function() {
+  MongoClient.connect(url, function(err, db) {
     var resultArray = [];
 
-    MongoClient.connect(url, function(err, db){
+    assert.equal(null, err);
+    var cursor = db
+      .db("ionic-jwt")
+      .collection("excercises")
+      .find();
+    cursor.forEach(
+      function(doc, err) {
         assert.equal(null, err);
-        var cursor = db.db("ionic-jwt").collection('users').find();
-        cursor.forEach(function(doc,err){
-            assert.equal(null, err);
-            resultArray.push(doc);
-        }, function(){
-            db.close();
-            res.json(resultArray)
-        });
-    });
-});
-test = function(){
-    MongoClient.connect(url, function(err, db) {
-        var resultArray = [];
-
-        assert.equal(null,err);
-        var cursor = db.db("ionic-jwt").collection('excercises').find();
-        cursor.forEach(function(doc,err){
-            assert.equal(null,err);
-            resultArray.push(doc);
-        }, function(){
-            db.close();
-            console.log(resultArray);
-        })
-    })
+        resultArray.push(doc);
+      },
+      function() {
+        db.close();
+        console.log(resultArray);
+      }
+    );
+  });
 };
 // test();
-routes.get('/get-exercises', function(req, res, next){
-    var resultArray = [];
+routes.get("/get-exercises", function(req, res, next) {
+  var resultArray = [];
 
-    MongoClient.connect(url, function(err, db){
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db
+      .db("ionic-jwt")
+      .collection("excercises")
+      .find();
+    cursor.forEach(
+      function(doc, err) {
         assert.equal(null, err);
-        var cursor = db.db("ionic-jwt").collection('excercises').find();
-        cursor.forEach(function(doc,err){
-            assert.equal(null, err);
-            resultArray.push(doc);
-        }, function(){
-            db.close();
-            res.json(resultArray)
-            
-        });
-    });
+        resultArray.push(doc);
+      },
+      function() {
+        db.close();
+        res.json(resultArray);
+      }
+    );
+  });
 });
-
 
 module.exports = routes;
